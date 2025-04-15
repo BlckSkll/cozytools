17147cbf818c61c7b66bb02a4c5248eb60b7f482
BlckSll <regina.oribeiro@outlook.com>
2025-04-15 19:43:33 -0300

    botões funcionais - lógica implementada

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let visor = document.getElementById("numeros-id");
 let segundos = 0;
+let intervaloID = null;
+let ativo = false
+
 function atualizarVisor(){
     let minutos = Math.floor(segundos / 60);
     let restoSegundos = segundos % 60;
@@ -13,4 +16,31 @@ function atualizarVisor(){
     segundos ++;
 }
 
-setInterval(atualizarVisor, 1000);
+
+document.getElementById("botoes_iniciar").addEventListener("click", iniciar);
+function iniciar(){
+    if (ativo === false){
+        intervaloID = setInterval(atualizarVisor, 1000);
+        ativo = true;
+    }
+
+}
+
+
+document.getElementById("botoes_pausar").addEventListener("click", pausar);
+function pausar(){
+    if(ativo === true){
+    clearInterval(intervaloID);
+    ativo = false;
+    }
+
+}
+
+document.getElementById("botoes_reiniciar").addEventListener("click", reiniciar);
+function reiniciar(){
+    clearInterval(intervaloID);
+    segundos = 0;
+    ativo = false;
+    atualizarVisor();
+}
+
